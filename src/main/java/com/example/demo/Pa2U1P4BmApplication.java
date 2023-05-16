e2e2fb2f4b506284ca8c69362a56b4b0a2a10869
Bryan Mullo <bfmullo@uce.edu.ec>
2023-05-15 19:34:39 -0500

    Taller 4

--- src/main/java/com/example/demo/Pa2U1P4BmApplication.java
+++ src/main/java/com/example/demo/Pa2U1P4BmApplication.java
@@ -1,8 +1,12 @@
 package com.example.demo;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.modelo.Profesor;
 
 @SpringBootApplication
 public class Pa2U1P4BmApplication implements CommandLineRunner{
@@ -15,6 +19,13 @@ public class Pa2U1P4BmApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Mi primer proyecto");
+		Profesor profe = new Profesor();
+		profe.setApellido("Mullo");
+		profe.setNombre("Bryan");
+		profe.setCedula("1753054285");
+		profe.setFechaNacimiento(LocalDateTime.now());
+		
+		System.out.println(profe);
 	}
 
 }
