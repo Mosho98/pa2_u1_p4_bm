9051fe7595d88119af0c31bde115a873eeea666b
Bryan Mullo <bfmullo@uce.edu.ec>
2023-05-16 19:42:11 -0500

    Taller 5

--- src/main/java/com/example/demo/Pa2U1P4BmApplication.java
+++ src/main/java/com/example/demo/Pa2U1P4BmApplication.java
@@ -2,15 +2,20 @@ package com.example.demo;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.demo.modelo.MatriculaCalculo;
 import com.example.demo.modelo.Profesor;
 
 @SpringBootApplication
 public class Pa2U1P4BmApplication implements CommandLineRunner{
 
+	@Autowired
+	private Profesor profe2;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P4BmApplication.class, args);
 	}
@@ -25,7 +30,21 @@ public class Pa2U1P4BmApplication implements CommandLineRunner{
 		profe.setCedula("1753054285");
 		profe.setFechaNacimiento(LocalDateTime.now());
 		
+		
 		System.out.println(profe);
+		System.out.println(profe.getApellido());
+		
+		System.out.println(profe2);
+		profe2.setApellido("Teran");
+		System.out.println(profe2.getApellido());
+		Profesor profe3;
+		profe3 = profe;
+		profe.setApellido("Paucar");
+		System.out.println(profe3);
+		
+		MatriculaCalculo mat = new MatriculaCalculo();
+		mat.realizarMatricula("1");
+	
 	}
 
 }
---
+++ src/main/java/com/example/demo/modelo/MateriaExtraordinaria.java
@@ -0,0 +1,24 @@
+package com.example.demo.modelo;
+
+public class MateriaExtraordinaria {
+
+	private String nombre;
+	private int cantidadCreditos;
+	
+	//GET Y SET
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getCantidadCreditos() {
+		return cantidadCreditos;
+	}
+	public void setCantidadCreditos(int cantidadCreditos) {
+		this.cantidadCreditos = cantidadCreditos;
+	}
+	
+	
+	
+}
